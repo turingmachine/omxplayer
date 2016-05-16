f0370724595ce35803e4bf7e194540c213f2bd4c
Simon Josi <me@yokto.net>
2016-05-16 15:06:41 +0200

    Raise output frequency of stats output (-s) to ~50 values per second
    This was taken care for in 116de6, but somehow it was now around 5
    values per second.

--- omxplayer.cpp
+++ omxplayer.cpp
@@ -1602,11 +1602,10 @@ int main(int argc, char *argv[])
       if(m_stats)
       {
         static int count;
-        if ((count++ & 7) == 0)
-           printf("M:%8.0f V:%6.2fs %6dk/%6dk A:%6.2f %6.02fs/%6.02fs Cv:%6dk Ca:%6dk                            \r", stamp,
-               video_fifo, (m_player_video.GetDecoderBufferSize()-m_player_video.GetDecoderFreeSpace())>>10, m_player_video.GetDecoderBufferSize()>>10,
-               audio_fifo, m_player_audio.GetDelay(), m_player_audio.GetCacheTotal(),
-               m_player_video.GetCached()>>10, m_player_audio.GetCached()>>10);
+        printf("M:%8.0f V:%6.2fs %6dk/%6dk A:%6.2f %6.02fs/%6.02fs Cv:%6dk Ca:%6dk                            \r", stamp,
+            video_fifo, (m_player_video.GetDecoderBufferSize()-m_player_video.GetDecoderFreeSpace())>>10, m_player_video.GetDecoderBufferSize()>>10,
+            audio_fifo, m_player_audio.GetDelay(), m_player_audio.GetCacheTotal(),
+            m_player_video.GetCached()>>10, m_player_audio.GetCached()>>10);
       }
 
       if(m_tv_show_info)
